09058ef26aaa36aa22838e1b8ca5ede079967b26
StarryLei <1018485883@qq.com>
2025-08-01 00:24:47 +0800

    update mcp client

--- mcp-client/server.py
+++ mcp-client/server.py
@@ -2,7 +2,7 @@
 Author: StarryLei 1018485883@qq.com
 Date: 2025-07-30 22:59:27
 LastEditors: StarryLei 1018485883@qq.com
-LastEditTime: 2025-07-30 23:35:15
+LastEditTime: 2025-07-31 22:45:15
 FilePath: \LangChainStudy\mcp-client\server.py
 Description: 创建一个简单的ＭＣＰ服务器示例
 '''
@@ -13,8 +13,13 @@ mcp = FastMCP()
 
 @mcp.tool()
 def get_welcome_message(name: str) -> str:
+    """
+    返回欢迎消息
+    :param name: 用户名
+    :return: 欢迎消息
+    """
     return f"Welcome to the MCP Server, {name}!"
 
 if __name__ == "__main__":
     # 启动MCP服务器
-    mcp.run()
+    mcp.run(transport="stdio")
